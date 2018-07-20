5d70a366ced1a147af0f3c333cde5585e4228dbe
abhijeetpatil5050 <abhijeetpatil5050@gmail.com>
2018-07-20 23:12:29 +0530

    this is child class

---
+++ Parent/src/main/java/com/abp/one/Address.java
@@ -0,0 +1,12 @@
+package com.abp.one;
+
+public class Address extends Emp {
+	
+	public  static String addName="Sangli";
+	
+	public static void main(String[] args) {
+		
+		System.out.println(id+"--"+name+"----"+addName);
+		
+	}
+}
---
+++ Parent/src/main/java/com/abp/one/Emp.java
@@ -0,0 +1,13 @@
+package com.abp.one;
+
+public class Emp {
+	public static int id =1;
+	protected static String name="Abhi";
+	public static void main(String[] args) {
+		
+		System.out.println(id+"--"+name);
+
+		
+	}
+	
+}
